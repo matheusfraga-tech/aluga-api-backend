707a875a3444a80f789c2c7da65120428a22a814
GustavoRossetti <128331107+GustavoRossetti@users.noreply.github.com>
2025-10-29 21:46:59 -0300

    correção virgula no CORS

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ from app.database.database import engine
 # -------------------- CORS --------------------
 origins = [
     "http://localhost:8000",
-    "http://127.0.0.1:8000"
-     "http://localhost:8081",
+    "http://127.0.0.1:8000",
+    "http://localhost:8081",
     "http://127.0.0.1:8081",
     "http://192.168.0.9:8000"
     ]
